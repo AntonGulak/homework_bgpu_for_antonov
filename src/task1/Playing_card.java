0ef06a3bb8349c7162b53ad700c5fa404c40c837
AntonGulak <55970327+AntonGulak@users.noreply.github.com>
2021-02-20 00:27:58 +0900

    random_relize

--- src/task1/Playing_card.java
+++ src/task1/Playing_card.java
@@ -14,51 +14,35 @@ public class Playing_card {
         rand();
     }
 
-    private static String[][] coloda_cart = new String[4][13];
+    private static String[] coloda_cart = new String[52];
 
     public void rand ()
     {
+        int tmp = 0;
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 13; j++)
             {
-                coloda_cart[i][j] = SUITS_LIST[i] + " " + RANK_LIST[j];
-                //System.out.println(coloda_cart[i][j]);
-            }
-
-        }
-
-        int temp1_rand = (int) (Math.random() * 255);
-        int temp2_rand = (int) (Math.random() * 255);
-
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 13; j++)
-            {
-                int t1_i = (i+temp1_rand)%4;
-                int t2_i = (i+temp2_rand)%4;
-
-                int t1_j = (j+temp1_rand)%13;
-                int t2_j = (j+temp2_rand)%13;
-
-                String temp = coloda_cart[t1_i][t1_j];
-                coloda_cart[t1_i][t1_j] = coloda_cart[t2_i][t2_j];
-                coloda_cart[t2_i][t2_j] = temp;
-
-            }
-
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 13; j++)
-            {
-                System.out.println(coloda_cart[i][j]);
+                coloda_cart[tmp++] = SUITS_LIST[i] + " " + RANK_LIST[j];
             }
 
         }
 
 
+        for (int i = 0; i < 52; i++)
+        {
+            var j = (int) (Math.random()*52);
+            var k = (int) (Math.random()*52);
+
+            String str_tmp = coloda_cart[j];
+            coloda_cart[j] = coloda_cart[k];
+            coloda_cart[k] = str_tmp;
+        }
+
+        for (int i = 0; i < 52; i++)
+        {
+            System.out.println(coloda_cart[i]);
+        }
 
 
     }
