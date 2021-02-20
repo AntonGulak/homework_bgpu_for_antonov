ac9f4bfb503d85b995df34aca07836b0a54b05f1
AntonGulak <55970327+AntonGulak@users.noreply.github.com>
2021-02-20 03:28:04 +0300

    task1_version_1

--- src/task2/Rock.java
+++ src/task2/Rock.java
@@ -1,4 +1,50 @@
 package task2;
 
 public class Rock {
+
+    //private int[] route;
+    private int max_height;
+    private int difference3_step;
+    private int length;
+
+    public int getMax_height() {
+        return max_height;
+    }
+
+    public void setMax_height(int max_height) {
+        this.max_height = max_height;
+    }
+
+    public int getDifference3_step() {
+        return difference3_step;
+    }
+
+    public void setDifference3_step(int difference3_step) {
+        this.difference3_step = difference3_step;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+        //int[] route = new int[length];
+    }
+
+    public void printRoute () {
+        for (int i = 0; i < length; i++)
+        {
+            int temp_random = (int) (Math.random() * max_height);
+
+
+        }
+
+    }
+
+    public Rock()
+    {
+
+    }
+
 }
--- src/task2/main.java
+++ src/task2/main.java
@@ -2,6 +2,18 @@ package task2;
 
 public class main {
     public static void main(String[] args) {
+        Rock test = new Rock();
+
+        final int length = 5;
+        test.setLength(length);
+
+        final int max_height = 24;
+        test.setMax_height(max_height);
+
+        test.printRoute();
+
+
+
 
     }
 }
